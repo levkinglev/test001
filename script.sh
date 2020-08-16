eae945ae62f475ac60ffb069f6b9fe32ba16a897
levkinglev <69664968+levkinglev@users.noreply.github.com>
2020-08-16 21:50:41 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
+##!/bin/bash
+#!/bin/sh
 
 echo "Test Container is running..."
 
-apt update && apt -y install mc lynx
-cp /usr/share/nginx/html/index.html /home
-sed 's!/usr/share/nginx/html!/home!' /etc/nginx/conf.d/default.conf
-service nginx reload
+#apt update && apt -y install mc lynx
+#cp /usr/share/nginx/html/index.html /home
+#sed 's!Welcome to!Welcome to HOME !' /home/index.html
+#sed 's!Welcome to!Welcome to USR !' /usr/share/nginx/html/index.html
+rm /usr/share/nginx/html/index.html
+ln -s /home/index.html /usr/share/nginx/html/
+#sed 's!/usr/share/nginx/html!/home!' /etc/nginx/conf.d/default.conf
+
+#service nginx reload
+
